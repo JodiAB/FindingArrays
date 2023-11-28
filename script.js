7ec4a8ed8383761e8d6e44a2b707ad8c0c6c99d0
JodiAB <jodiabrahams9@gmail.com>
2023-11-28 09:40:15 +0200

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+// Wait for the webpage to finish loading
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Define a function to find unique numbers from two lists
     function difference(arr1, arr2) {
-        const combinedArray = arr1.concat(arr2);
-        const uniqueElementsSet = new Set(combinedArray);
-        const sortedUniqueElements = Array.from(uniqueElementsSet).map(Number).sort((a, b) => a - b);
-        return sortedUniqueElements;
+        // Combine the two lists into one
+        const combinedList = arr1.concat(arr2);
+        
+        // Remove duplicate numbers to get unique numbers
+        const uniqueNumbers = Array.from(new Set(combinedList)).map(Number);
+        
+        // Sort the unique numbers in ascending order
+        const sortedUniqueNumbers = uniqueNumbers.sort((a, b) => a - b);
+        
+        // Return the sorted list of unique numbers
+        return sortedUniqueNumbers;
     }
 
+    // Add a click event listener to a button on the webpage
     document.getElementById('findUniqueButton').addEventListener('click', function () {
-        const array1 = document.getElementById('array1').value.split(',').map(item => item.trim());
-        const array2 = document.getElementById('array2').value.split(',').map(item => item.trim());
+        // Get input values from two text boxes on the webpage
+        const list1 = document.getElementById('array1').value.split(',').map(item => item.trim());
+        const list2 = document.getElementById('array2').value.split(',').map(item => item.trim());
 
-        const uniqueElements = difference(array1, array2);
+        // Find unique numbers from the input lists
+        const uniqueNumbers = difference(list1, list2);
 
-        document.getElementById('result').innerHTML = 'Unique Elements (sorted): ' + uniqueElements.join(', ');
+        // Display the result on the webpage
+        document.getElementById('result').innerHTML = 'Unique Numbers (sorted): ' + uniqueNumbers.join(', ');
     });
 });
